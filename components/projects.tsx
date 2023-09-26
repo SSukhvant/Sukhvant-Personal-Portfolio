67a675012482c607de9de357a6fa45a62cc3edb9
Sukhvant Singh <sukhvantsingh581998@gmail.com>
2023-09-26 14:23:40 +0530

    Updated

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import SectionHeading from "./section-heading";
 import { projectsData } from "@/lib/data";
 import Project from "./project";
@@ -8,16 +8,22 @@ import { useSectionInView } from "@/lib/hooks";
 
 export default function Projects() {
   const { ref } = useSectionInView("Projects", 0.5);
+  const [visible, setVisible] = useState(3);
+
+  const showMoreItems = () => {
+    setVisible((prevValue) => prevValue + 1);
+  };
 
   return (
     <section ref={ref} id="projects" className="scroll-mt-28 mb-28">
       <SectionHeading>My projects</SectionHeading>
       <div>
-        {projectsData.map((project, index) => (
+        {projectsData.slice(0, visible).map((project, index) => (
           <React.Fragment key={index}>
             <Project {...project} />
           </React.Fragment>
         ))}
+        <button className="btn-highlight-none group bg-white px-7 py-3 mx-auto flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10" onClick={showMoreItems}>Load More</button>
       </div>
     </section>
   );
