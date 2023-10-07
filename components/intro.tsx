18eda608b6b5c32630fd4baa6a9f2ef848892f9b
Sukhvant Singh <sukhvantsingh581998@gmail.com>
2023-10-08 03:54:06 +0530

    Updated

--- components/intro.tsx
+++ components/intro.tsx
@@ -91,7 +91,7 @@ export default function Intro() {
 
         <a
           className="btn-highlight-none group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="/Sukhvant_Singh.pdf"
+          href="/Sukhvant_Singh_CV.pdf"
           download
         >
           Download CV{" "}
