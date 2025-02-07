e1e54012c4865563a05831c699e1b5d80613b9e9
Sukhvant Singh <sukhvantsingh581998@gmail.com>
2025-02-07 14:42:21 +0530

    Updated

--- components/intro.tsx
+++ components/intro.tsx
@@ -9,6 +9,7 @@ import { HiDownload } from "react-icons/hi";
 import { FaGithubSquare, FaLinkedin } from "react-icons/fa";
 import { useSectionInView } from "@/lib/hooks";
 import { useActiveSectionContext } from "@/context/active-section-context";
+import profile from '@/public/sukh.png';
 
 export default function Intro() {
   const { ref } = useSectionInView("Home", 0.5);
@@ -31,7 +32,7 @@ export default function Intro() {
             }}
           >
             <Image
-              src="/sukh.png"
+              src={profile}
               alt="Sukhvant Singh"
               width="192"
               height="192"
